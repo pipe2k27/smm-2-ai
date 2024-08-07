e6ee56464feb80af5280e127ac8dd8e2b412303b
emilioledo <fernandezledoemilio@gmail.com>
2024-08-07 15:22:07 -0300

    loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Container } from '@mui/material';
 import { Header } from './components/layout/Header.component';
 import './App.css';
+import { LoadingSpinner } from './components/UI';
 import { SJ210000 } from './pages/index';
 
 export const App = (): React.ReactElement => (
   <Container maxWidth={false} sx={{ maxWidth: '1024px' }} disableGutters>
     <Header />
+    <LoadingSpinner />
     <SJ210000 />
   </Container>
 );
